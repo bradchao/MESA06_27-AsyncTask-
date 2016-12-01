03a5a76750174069eb93ca1c80020e6a4ca75b93
Brad Chao <brad@brad.tw>
2016-12-01 11:43:13 +0800

    v1

---
+++ app/src/main/java/com/example/user/brad27/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.user.brad27;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View v){
+
+    }
+
+
+    private class MyAsyncTask extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aNull) {
+            super.onPostExecute(aNull);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onCancelled(Void aNull) {
+            super.onCancelled(aNull);
+        }
+
+        @Override
+        protected Void doInBackground(Void... nulls) {
+            return null;
+        }
+    }
+}
